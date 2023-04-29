080dee6fcb5c146704c5ed8c6036381b9bf57084
Blake Curtsinger <ccurtsin@vols.utk.edu>
2023-04-29 18:01:49 -0400

    Modeling script mainly finished, could use some tuning later on

--- Images.py
+++ Images.py
@@ -1,116 +1,91 @@
 import tensorflow as tf
-import pandas as pd
-import json
-import os
-import shutil
+import h5py
 import matplotlib.pyplot as plt
-
-#k+1 softmax layers where the last layer predicts the len of numbers in the house number
+import numpy as np
+from keras.utils import np_utils
 
 # Set Image Directory (change this to accomodate yourself)
 main_dir = '/Users/blakecurtsinger/Desktop/BZAN554/Group_Assignments/group_assignment_3/SVHN'
-train_dir = main_dir + '/train_cropped'
-test_dir = main_dir + '/test_cropped'
+train_dir = main_dir + '/train'
+test_dir = main_dir + '/test'
 
-## Metadata
+##Load pre-processed data
+# Open the HDF5 file containing the datasets
+with h5py.File(main_dir + '/SVHN_multi_digit_norm_grayscale.h5','r') as h5f:
+    X_train = h5f['X_train'][:]
+    y_train = h5f['y_train'][:]
+    X_val = h5f['X_val'][:]
+    y_val = h5f['y_val'][:]
+    X_test = h5f['X_test'][:]
+    y_test = h5f['y_test'][:]
 
-# rename .json files
-os.rename(main_dir + "/train/digitStruct.json", main_dir + "/train/digitStruct_train.json")
-os.rename(main_dir + "/test/digitStruct.json", main_dir + "/test/digitStruct_test.json")
+print('Training set', X_train.shape, y_train.shape)
+print('Validation set', X_val.shape, y_val.shape)
+print('Test set', X_test.shape, y_test.shape)
 
-# Set directories to move .json files
-source_train = main_dir + "/train/digitStruct_train.json"
-source_test = main_dir + "/test/digitStruct_test.json"
-destination = main_dir + "/metadata"
+## Plot images
+def plot_images(images, nrows, ncols, cls_true, cls_pred=None):
+    """ Helper function for plotting nrows * ncols images
+    """
+    fig, axes = plt.subplots(nrows, ncols, figsize=(16, 2*nrows))
+    for i, ax in enumerate(axes.flat): 
+        # Pretty string with actual label
+        true_number = ''.join(str(x) for x in cls_true[i] if x != 10)
+        if cls_pred is None:
+            title = "Label: {0}".format(true_number)
+        else:
+            # Pretty string with predicted label
+            pred_number = ''.join(str(x) for x in cls_pred[i] if x != 10)
+            title = "Label: {0}, Pred: {1}".format(true_number, pred_number)  
+            
+        if images[i].shape == (32, 32, 3):
+            ax.imshow(images[i])
+        else:
+            ax.imshow(images[i,:,:,0], cmap="gray")
+        #ax.imshow(images[i])
+        ax.set_title(title)   
+        ax.set_xticks([]); ax.set_yticks([])
 
-# Make metadata folder, move .json files
-os.makedirs(destination, exist_ok=True)
-shutil.move(source_train, destination)
-shutil.move(source_test, destination)
-
-# Load JSON training file with image metadata
-with open(os.path.join(main_dir, 'metadata', 'digitStruct_train.json'), 'r') as f:
-    metadata_train = json.load(f)
-
-# Load JSON test file with image metadata
-with open(os.path.join(main_dir, 'metadata', 'digitStruct_test.json'), 'r') as f:
-    metadata_test = json.load(f)
-
-#Function for getting image labels. Returns a dictionary with the keys as the filenames and their associated labels
-def get_image_labels(metadata):
-    image_labels = {}
-    for obj in metadata:
-        filename = obj['filename']
-        num_digits = len(obj['boxes'])
-        label = [int(obj['boxes'][i]['label']) for i in range(num_digits)]
-        image_labels[filename] = label
-    return image_labels
-
-train_labels = get_image_labels(metadata_train)
-test_labels = get_image_labels(metadata_test)
-
-train_labels.values
-
-# Height and Width for generator
-img_height = 256
-img_width = 256
-
-# Image generators
-train_generator = tf.keras.utils.image_dataset_from_directory(
-    train_dir,
-    labels=None,
-    label_mode=None,
-    class_names=None,
-    color_mode='grayscale',
-    batch_size=32,
-    image_size=(img_height, img_width),
-    shuffle=True,
-    seed=123,
-    validation_split=None,
-    subset=None,
-    interpolation='bilinear',
-    follow_links=False,
-    crop_to_aspect_ratio=False
-)
-
-test_generator = tf.keras.utils.image_dataset_from_directory(
-    test_dir,
-    labels=None,
-    label_mode=None,
-    class_names=None,
-    color_mode='grayscale',
-    batch_size=32,
-    image_size=(img_height, img_width),
-    shuffle=True,
-    seed=123,
-    validation_split=None,
-    subset=None,
-    interpolation='bilinear',
-    follow_links=False,
-    crop_to_aspect_ratio=False
-)
-
-# Image shape
-for image_batch in test_generator:
-  print(image_batch.shape)
-  break
-
-#This shows that the generator works. Plot 9 training images
-plt.figure(figsize=(10, 10))
-for images in train_generator.take(1):
-  for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(images[i].numpy().astype("uint8"))
-    plt.axis("off")
+plot_images(X_train, 2, 4, y_train)
 plt.show()
 
-## Pre-processing and Modeling
 
-inputs = tf.keras.layers.Input(shape=(img_height, img_width, 1), name='input') 
-#rescale input layer between 0 and 1
-rescaled = tf.keras.layers.Rescaling(scale=1./255)(inputs)
+possible_classes = 11
+def convert_labels(labels):
+    
+    # As per Keras conventions, the multiple labels need to be of the form [array_digit1,...5]
+    # Each digit array will be of shape (60000,11)
+        
+    # Declare output ndarrays
+    # 5 for digits, 11 for possible classes  
+    dig0_arr = np.ndarray(shape=(len(labels),possible_classes))
+    dig1_arr = np.ndarray(shape=(len(labels),possible_classes))
+    dig2_arr = np.ndarray(shape=(len(labels),possible_classes))
+    dig3_arr = np.ndarray(shape=(len(labels),possible_classes))
+    dig4_arr = np.ndarray(shape=(len(labels),possible_classes))
+    
+    for index,label in enumerate(labels):
+        
+        # Using np_utils from keras to OHE the labels in the image
+        dig0_arr[index,:] = np_utils.to_categorical(label[0],possible_classes)
+        dig1_arr[index,:] = np_utils.to_categorical(label[1],possible_classes)
+        dig2_arr[index,:] = np_utils.to_categorical(label[2],possible_classes)
+        dig3_arr[index,:] = np_utils.to_categorical(label[3],possible_classes)
+        dig4_arr[index,:] = np_utils.to_categorical(label[4],possible_classes)
+        
+    return [dig0_arr,dig1_arr,dig2_arr,dig3_arr,dig4_arr]
+
+y_train = convert_labels(y_train)
+y_test = convert_labels(y_test)
+y_val = convert_labels(y_val)
+
+np.shape(y_train[0])
+
+
+#Conv layers requires 4 dimensional input (i.e., if we input three dimensions and add batch dim we get 4D)
+inputs = tf.keras.layers.Input(shape=(32,32,1), name='input') 
 #Conv2D layer (2D does not refer to gray scale (a PET scan would be 3D))
-x = tf.keras.layers.Conv2D(filters=64,kernel_size = 7, strides = 1, padding = "same", activation = "relu")(rescaled)
+x = tf.keras.layers.Conv2D(filters=64,kernel_size = 7, strides = 1, padding = "same", activation = "relu")(inputs)
 #MaxPooling2D: pool_size is window size over which to take the max
 x = tf.keras.layers.MaxPooling2D(pool_size = 2, strides = 2, padding = "valid")(x)
 x = tf.keras.layers.Conv2D(filters=128,kernel_size = 3, strides = 1, padding = "same", activation = "relu")(x)
@@ -123,18 +98,31 @@ x = tf.keras.layers.MaxPooling2D(pool_size = 2, strides = 2, padding = "valid")(
 x = tf.keras.layers.Flatten()(x)
 x = tf.keras.layers.Dense(128, activation = 'relu')(x)
 x = tf.keras.layers.Dense(64, activation = 'relu')(x)
-yhat1 = tf.keras.layers.Dense(10, activation = 'softmax')(x)
+yhat1 = tf.keras.layers.Dense(11, activation = 'softmax', name = "yhat1")(x)
+yhat2 = tf.keras.layers.Dense(11, activation = 'softmax', name = "yhat2")(x)
+yhat3 = tf.keras.layers.Dense(11, activation = 'softmax', name = "yhat3")(x)
+yhat4 = tf.keras.layers.Dense(11, activation = 'softmax', name = "yhat4")(x)
+yhat5 = tf.keras.layers.Dense(11, activation = 'softmax', name = "yhat5")(x)
 
-model = tf.keras.Model(inputs = inputs, outputs = yhat)
+
+#Why do we stack two convolutional layers followed by a pooling layer, as opposed to having each convolutional layer followed by a pooling layer?
+# Answer: every convolutional layer creates a number of feature maps (e.g,64) that are individually connected to the previous layer.
+# By stacking two convolutional layers before inserting a pooling layer we allow the second convolutional layer to learn from the noisy signal, as opposed to the clean signal.
+
+model = tf.keras.Model(inputs = inputs, outputs = [yhat1,yhat2,yhat3,yhat4,yhat5])
 model.summary()
 
-model.compile(loss = 'sparse_categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001))
+
+#Compile model
+model.compile(loss = "categorical_crossentropy", optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001))
 
 #Fit model
-history = model.fit(
-    train_generator,
-    epochs = 100,
-    callbacks = [
+model.fit(x=X_train,
+          y=y_train,
+          epochs=10,
+          batch_size=32,
+          validation_data=(X_val, y_val),
+              callbacks = [
         tf.keras.callbacks.EarlyStopping(
             monitor = "val_loss",
             patience = 5,
@@ -144,5 +132,4 @@ history = model.fit(
             monitor = "val_loss",
             patience = 3
         )
-    ]
-)
+    ])
